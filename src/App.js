3cc60f54fe1ab5632c48822f35dc6537a78631f2
Jacob Templeton <jacobtempleton@Jacobs-MacBook-Pro.local>
2021-06-14 12:33:56 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,218 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case "STORIES_FETCH_INIT":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case "STORIES_FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case "STORIES_FETCH_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case "REMOVE_STORY":
+      return {
+        ...state,
+        data: state.data.filter(
+          (story) => action.payload.objectID !== story.objectID
+        ),
+      };
+    default:
+      throw new Error();
+  }
+};
+
+const useSemiPersistentState = (key, initialState) => {
+  // INITIALLY THERE IS AN EMPTY STATE ('')
+  // HANDLECHANGE FUNCTION SETS THE EVENT VALUE = TO setSearchTerm
+  // BY DOING SO IT SETS searchTerm TO THE SAME VALUE
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  // USEEFFECT HOOK TAKES TWO ARGUMENTS-- A FUNCTION WHERE THE SIDE-EFFECT
+  // TAKES PLACE (HERE THE SIDE-EFFECT IS WHEN SEARCH TERM IS SET IN BROWSERS
+  // LOCAL STORAGE), AND OPTIONAL SECONDARY ARGUMENT SAYING TO RUN THE FUNC
+  // EVERY TIME THE VALUE IN THE ARRAY CHANGES (HERE IT IS SEARCHTERM)
+  // HAVING AN EMPTY ARRAY MEANS THE FUNCTION FOR THE SIDE-EFFECT ONLY RUNS
+  // ONCE, WHEN THE COMPONENT RENDERS FOR THE FIRST TIME
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
+
+  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
+
+  const [stories, dispatchStories] = React.useReducer(storiesReducer, {
+    data: [],
+    isLoading: false,
+    isError: false,
+  });
+
+  const handleFetchStories = React.useCallback(async () => {
+    dispatchStories({ type: "STORIES_FETCH_INIT" });
+
+    try {
+      const result = await axios.get(url);
+
+      dispatchStories({
+        type: "STORIES_FETCH_SUCCESS",
+        payload: result.data.hits,
+      });
+    } catch {
+      dispatchStories({ TYPE: "STORIES_FETCH_FAILURE" });
+    }
+  }, [url]);
+
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
+
+  const handleRemoveStory = (item) => {
+    dispatchStories({
+      type: "REMOVE_STORY",
+      payload: item,
+    });
+  };
+
+  // THIS IS A CALLBACK HANDLER-- THE SEARCH FEATURE WAS MADE A NEW Component
+  // BECAUSE IT IS A CHILD OF App, APP CANT SEE THE searchTerm
+  // THE CALLBACK FUNCTION IS WRITTEN IN APP, SENT TO SEARCH AS A PROP,
+  // THE EVENT IS PASSED BACK UP TO APP WHERE IT CAN BE USED
+  const handleSearchInput = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+
+    event.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="headline-primary">HackerNews Story Search</h1>
+
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
+
+      {/* If isError is true the <p> will be displayed  */}
+      {stories.isError && <p>Something went wrong...</p>}
+
+      {stories.isLoading ? (
+        <p>Loading....</p>
+      ) : (
+        //* Pass stories array to List component w/ list prop
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
+      )}
     </div>
   );
 }
+
+const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => {
+  return (
+    <form onSubmit={onSearchSubmit} className="search-form">
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={onSearchInput}
+      >
+        <strong>Search: </strong>
+      </InputWithLabel>
+      <button
+        type="submit"
+        disabled={!searchTerm}
+        className="button button_large"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
+
+const InputWithLabel = ({
+  id,
+  value,
+  type = "text",
+  onInputChange,
+  isFocused,
+  children,
+}) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+  return (
+    <>
+      <label htmlFor={id} className="label">
+        {children}
+      </label>
+      &nbsp;
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        autoFocus={isFocused}
+        onChange={onInputChange}
+        className="input"
+      />
+    </>
+  );
+};
+
+const List = ({ list, onRemoveItem }) =>
+  list.map((item) => (
+    <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+  ));
+
+const Item = ({ item, onRemoveItem }) => {
+  return (
+    <div className="item">
+      <span style={{ width: "40%" }}>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span style={{ width: "30%" }}>{item.author}</span>
+      <span style={{ width: "10%" }}>{item.num_comments}</span>
+      <span style={{ width: "10%" }}>{item.points}</span>
+      <span style={{ width: "10%" }}>
+        <button
+          className="button button_small"
+          type="button"
+          onClick={() => onRemoveItem(item)}
+        >
+          Dismiss
+        </button>
+      </span>
+    </div>
+  );
+};
 
 export default App;
